1a6750503c2b8d02a2bc5822372e7ff0e34314c5
Niouzty <epembelefuala@iut.univ-paris8.fr>
2025-09-07 23:58:14 +0200

    feat(frontend): affichage produits (carte-client) avec ProductService

--- frontend/app/src/app/features/products/carte-client/carte-client/carte-client.component.ts
+++ frontend/app/src/app/features/products/carte-client/carte-client/carte-client.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Product } from '../../../../core/models/product.model';
+import { ProductService } from '../../../../core/services/product.service';
 
 @Component({
   selector: 'app-carte-client',
@@ -10,8 +11,29 @@ import { Product } from '../../../../core/models/product.model';
 })
 
 export class CarteClientComponent {
-    products: Product[] = [];
+  products: Product[] = [];   
+  loading: boolean = true;    
+  error: string | null = null; 
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loading = true;   
+    this.error = null;     
+  
+    this.productService.getProduits().subscribe({
+      next: (data) => {
+        this.products = data;
+        this.loading = false; 
+      },
+      error: (err) => {
+        console.error("Erreur:", err);
+        this.error = "Impossible de charger les produits"; 
+        this.loading = false; 
+      }
+    });
+  }
+  
 
 
 }
