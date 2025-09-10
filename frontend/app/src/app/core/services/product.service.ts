38d06d3424a4288772621b30486c88bc7e5d85da
Niouzty <epembelefuala@iut.univ-paris8.fr>
2025-09-10 09:48:51 +0200

    mise à jour fichier

--- frontend/app/src/app/core/services/product.service.ts
+++ frontend/app/src/app/core/services/product.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = `${environment.apiUrl}/products/`;
+  private apiUrl = `${environment.apiUrl}/products`;
 
   constructor(private http: HttpClient) {}
 
