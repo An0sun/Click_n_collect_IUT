96e97d3ed972b7c415688c3a81bc903bb9b1ec54
An0sun <Kyllian.meridja@gmail.com>
2025-11-05 13:56:01 +0100

    feature(products-images): image par défaut de produits changée

--- frontend/app/src/app/features/products/component/customer-card/customer-card.component.ts
+++ frontend/app/src/app/features/products/component/customer-card/customer-card.component.ts
@@ -14,7 +14,7 @@ import { Router } from '@angular/router';
 
 export class CustomerCardComponent {
   @Input() product!: Product;
-  placeholderUrl = 'https://images.unsplash.com/photo-1757743066455-4b5b471cdb80?q=80&w=600&auto=format&fit=crop';
+  placeholderUrl = 'https://images.unsplash.com/photo-1574282341541-0b3ed53adcd2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=688';
   constructor(private cartService: CartService, private router: Router){}
 
   addToCart(product: Product) {
